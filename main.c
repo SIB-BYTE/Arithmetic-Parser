d20de4a925a6166a89b1461a53166a702afbdb1b
SIB-BYTE <97762248+SIB-BYTE@users.noreply.github.com>
2022-01-18 23:30:05 +0000

    Update main.c

--- main.c
+++ main.c
@@ -9,16 +9,42 @@
 #include <stdio.h>
 #include "parser.h"
 
+#define TRUE 1
+#define BLUE printf("\x1b[1;34m")
+#define CONSTRUCTOR __attribute__((constructor))
+
+// Yes I know that a banner is kinda useless and not-needed but I felt like doing one so why not?
+void CONSTRUCTOR banner_display(void)
+{
+	char *banner[] = {
+	"  ██████╗  █████╗ ██╗   ██╗ ",
+	" ██╔════╝ ██╔══██╗╚██╗ ██╔╝ ",
+	" ██║  ███╗███████║ ╚████╔╝  ",
+	" ██║   ██║██╔══██║  ╚██╔╝   ",
+	" ╚██████╔╝██║  ██║   ██║    ",
+ 	"  ╚═════╝ ╚═╝  ╚═╝   ╚═╝    "
+	};
+
+	int len = sizeof(banner) / sizeof((*banner));
+
+	BLUE;
+	for(int i = 0; i < len; i++)
+		printf("%s\n", banner[i]);
+}
+
 int main(void)
 {
 	char expr[64];
 
-	printf("Provide a grammar: ");
-	fgets(expr, sizeof(expr), stdin);
+	while(TRUE)
+	{
+		printf("Provide a valid grammar: ");
+		fgets(expr, sizeof(expr), stdin);
 
-	lexer_t *tokenize = lexer(expr);
-	printf("Result of grammar -> {%d}\n", parse(tokenize));
+		lexer_t *tokenize = lexer(expr);
+		printf("Result of grammar -> {%d}\n", parse(tokenize));
 
-	destroy_all_tokens(tokenize);
-	lexer_destroy(tokenize);
+		destroy_all_tokens(tokenize);
+		lexer_destroy(tokenize);
+	}
 }
