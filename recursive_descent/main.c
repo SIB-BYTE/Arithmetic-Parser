fb3da901076637cb856d665080d9b91cd4231988
SIB-BYTE <97762248+SIB-BYTE@users.noreply.github.com>
2022-02-20 20:09:48 +0000

    Create main.c

---
+++ recursive_descent/main.c
@@ -0,0 +1,29 @@
+/*
+<expr> ::= <term> ( + | - ) <expr> | <term>
+<term> ::= <power> ( * | / | % ) <term> | <power>
+<power> ::= <unary> ^ <power> | <unary>
+<unary> ::= - <unary> | <factor>
+<factor> ::= ( <expr> ) | <integer>
+*/
+
+#include <stdio.h>
+#include "parser.h"
+
+#define TRUE 1
+
+int main(void)
+{
+	char expr[64];
+
+	while(TRUE)
+	{
+		printf("Provide a valid grammar: ");
+		fgets(expr, sizeof(expr), stdin);
+
+		lexer_t *tokenize = lexer(expr);
+		printf("Result of grammar -> {%lf}\n", parse(tokenize));
+
+		destroy_all_tokens(tokenize);
+		lexer_destroy(tokenize);
+	}
+}
